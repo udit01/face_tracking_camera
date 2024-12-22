396ba5a4e8288670b8ed5f290cf733000dccbe90
Hwia Kim <88076768+HwiaKim@users.noreply.github.com>
2024-12-22 09:47:59 +0000

    testing

--- Camera_tracking/Camera_tracking.ino
+++ Camera_tracking/Camera_tracking.ino
@@ -5,12 +5,23 @@
 
 #include <Servo.h>
 
-Servo panServo;
-Servo tiltServo; 
+//Servo panServo;
+//Servo tiltServo; 
+Servo upper1;
+Servo upper2;
+Servo lower3;
+Servo lower4;
+Servo lower5;
+//
+//byte redledPin = 2;
+//byte yellowledPin = 3;
+//byte greenledPin = 4;
+//
+//
+//byte redledPin = 6;
+//byte yellowledPin = 7;
+//byte greenledPin = 8;
 
-byte redledPin = 2;
-byte yellowledPin = 3;
-byte greenledPin = 4;
 
 const byte buffSize = 40;
 char inputBuffer[buffSize];
@@ -20,26 +31,52 @@ byte bytesRecvd = 0;
 boolean readInProgress = false;
 boolean newDataFromPC = false;
 
-float panServoAngle = 90.0;
-float tiltServoAngle = 90.0;
+//float panServoAngle = 90.0;
+//float tiltServoAngle = 90.0;
+
+
+int servo1_good_angle = 145;
+int servo2_good_angle = 110;
+int servo3_good_angle = 30;
+int servo4_good_angle = 145;
+int servo5_good_angle = 30;
+
+
+float servo1_angle = servo1_good_angle;
+float servo2_angle = servo2_good_angle;
+float servo3_angle = servo3_good_angle;
+float servo4_angle = servo4_good_angle;
+float servo5_angle = servo5_good_angle;
+
+
+
 int LED_state = 0;
+
+
 
 //8=============D
 
 void setup() {
-  Serial.begin(115200);
-  
-  panServo.attach(8);
-  tiltServo.attach(9);
-
-  pinMode(redledPin, OUTPUT);
-  pinMode(yellowledPin, OUTPUT);
-  pinMode(greenledPin, OUTPUT);
-  
+  while (!Serial);
+  Serial.begin(9600);
+//   while (!Serial)
+//   {;}
+//  panServo.attach(8);
+//  tiltServo.attach(9);
+    upper1.attach(1);
+    upper2.attach(2);
+    lower3.attach(3);
+    lower4.attach(4);
+    lower5.attach(5);
+    
+//  pinMode(redledPin, OUTPUT);
+//  pinMode(yellowledPin, OUTPUT);
+//  pinMode(greenledPin, OUTPUT);
+//  
   //moveServo();
   start_sequence();
 
-  delay(200);
+  delay(5000);
   
   Serial.println("<Hasta la vista baby>"); // send message to computer
 }
@@ -50,7 +87,7 @@ void loop() {
   getDataFromPC();
   replyToPC();
   moveServo();
-  setLED();
+//  setLED();
 }
 
 //8=============D
@@ -92,10 +129,19 @@ void processData() // for data type "<float, float, int>"
   char * strtokIndx; // this is used by strtok() as an index
 
    strtokIndx = strtok(inputBuffer,",");      // get the first part
-   panServoAngle = atof(strtokIndx);         // convert this part to a float
+   servo1_angle = atof(strtokIndx);         // convert this part to a float
 
    strtokIndx = strtok(NULL,",");          // get the second part(this continues where the previous call left off)
-   tiltServoAngle = atof(strtokIndx);     // convert this part to a float
+   servo2_angle = atof(strtokIndx);     // convert this part to a float
+
+   strtokIndx = strtok(NULL,",");          // get the second part(this continues where the previous call left off)
+   servo3_angle = atof(strtokIndx);     // convert this part to a float
+   
+   strtokIndx = strtok(NULL,",");          // get the second part(this continues where the previous call left off)
+   servo4_angle = atof(strtokIndx);     // convert this part to a float
+   
+   strtokIndx = strtok(NULL,",");          // get the second part(this continues where the previous call left off)
+   servo5_angle = atof(strtokIndx);     // convert this part to a float
 
    strtokIndx = strtok(NULL, ",");      // get the last part
    LED_state = atoi(strtokIndx);          // convert this part to an integer (string to int)
@@ -108,9 +154,15 @@ void replyToPC() {
   if (newDataFromPC) {
     newDataFromPC = false;
     Serial.print("<");
-    Serial.print(panServo.read());
+    Serial.print(upper1.read());
     Serial.print(",");
-    Serial.print(tiltServo.read());
+    Serial.print(upper2.read());
+    Serial.print(",");
+    Serial.print(lower3.read());
+    Serial.print(",");
+    Serial.print(lower4.read());
+    Serial.print(",");
+    Serial.print(lower5.read());
     Serial.println(">");
   }
 }
@@ -119,67 +171,76 @@ void replyToPC() {
 
 void moveServo() 
 {
-  panServo.write(panServoAngle);
-  tiltServo.write(tiltServoAngle);
+//  panServo.write(panServoAngle);
+//  tiltServo.write(tiltServoAngle);
+  upper1.write(servo1_angle);
+  upper2.write(servo2_angle);
+  lower3.write(servo3_angle);
+  lower4.write(servo4_angle);
+  lower5.write(servo5_angle);
 }
-
-void setLED()
-{
-  if(LED_state == 2){
-    digitalWrite(redledPin, LOW);
-    digitalWrite(yellowledPin, HIGH);
-    digitalWrite(greenledPin, LOW);
-    }
-  else if(LED_state == 1){
-    digitalWrite(redledPin, LOW);
-    digitalWrite(yellowledPin, LOW);
-    digitalWrite(greenledPin, HIGH);    
-    }
-  else if(LED_state == 0){
-    digitalWrite(redledPin, HIGH);
-    digitalWrite(yellowledPin, LOW);
-    digitalWrite(greenledPin, LOW);  
-    }  
-  else if(LED_state == 3){
-    digitalWrite(redledPin, HIGH);
-    digitalWrite(yellowledPin, HIGH);
-    digitalWrite(greenledPin, HIGH);  
-    }  
-  else{
-    digitalWrite(redledPin, LOW);
-    digitalWrite(yellowledPin, LOW);
-    digitalWrite(greenledPin, LOW);    
-    }
-  
-  }
-
-//8=============D
+//
+//void setLED()
+//{
+//  if(LED_state == 2){
+//    digitalWrite(redledPin, LOW);
+//    digitalWrite(yellowledPin, HIGH);
+//    digitalWrite(greenledPin, LOW);
+//    }
+//  else if(LED_state == 1){
+//    digitalWrite(redledPin, LOW);
+//    digitalWrite(yellowledPin, LOW);
+//    digitalWrite(greenledPin, HIGH);    
+//    }
+//  else if(LED_state == 0){
+//    digitalWrite(redledPin, HIGH);
+//    digitalWrite(yellowledPin, LOW);
+//    digitalWrite(greenledPin, LOW);  
+//    }  
+//  else if(LED_state == 3){
+//    digitalWrite(redledPin, HIGH);
+//    digitalWrite(yellowledPin, HIGH);
+//    digitalWrite(greenledPin, HIGH);  
+//    }  
+//  else{
+//    digitalWrite(redledPin, LOW);
+//    digitalWrite(yellowledPin, LOW);
+//    digitalWrite(greenledPin, LOW);    
+//    }
+//  
+//  }
+//
+////8=============D
 
   void start_sequence()
   {
-    panServo.write(90);
-    tiltServo.write(90);
-    delay(300);
+    upper1.write(servo1_good_angle);
+    upper2.write(servo2_good_angle);
+    lower3.write(servo3_good_angle);
+    lower4.write(servo4_good_angle);
+    lower5.write(servo5_good_angle);
+    
+    delay(3000);
 
-  
-    digitalWrite(redledPin, HIGH);
-    delay(100);
-    digitalWrite(redledPin, LOW);
-    digitalWrite(yellowledPin, HIGH);
-    delay(100);
-    digitalWrite(yellowledPin, LOW);
-    digitalWrite(greenledPin, HIGH);
-    delay(100);
-  
-    digitalWrite(redledPin, LOW);
-    digitalWrite(yellowledPin, LOW);
-    digitalWrite(greenledPin, LOW);
-    delay(100);
-    digitalWrite(redledPin, HIGH);
-    digitalWrite(yellowledPin, HIGH);
-    digitalWrite(greenledPin, HIGH);
-    delay(100);
-    digitalWrite(redledPin, LOW);
-    digitalWrite(yellowledPin, LOW);
-    digitalWrite(greenledPin, LOW);
+//  
+//    digitalWrite(redledPin, HIGH);
+//    delay(100);
+//    digitalWrite(redledPin, LOW);
+//    digitalWrite(yellowledPin, HIGH);
+//    delay(100);
+//    digitalWrite(yellowledPin, LOW);
+//    digitalWrite(greenledPin, HIGH);
+//    delay(100);
+//  
+//    digitalWrite(redledPin, LOW);
+//    digitalWrite(yellowledPin, LOW);
+//    digitalWrite(greenledPin, LOW);
+//    delay(100);
+//    digitalWrite(redledPin, HIGH);
+//    digitalWrite(yellowledPin, HIGH);
+//    digitalWrite(greenledPin, HIGH);
+//    delay(100);
+//    digitalWrite(redledPin, LOW);
+//    digitalWrite(yellowledPin, LOW);
+//    digitalWrite(greenledPin, LOW);
     }
